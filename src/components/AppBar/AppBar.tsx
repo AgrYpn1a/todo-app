2a45bcd69722c2ed673d6a8e038efe7c85abe80e
Rastko Tojagic <tojagic.rastko@gmail.com>
2022-07-31 22:30:15 +0200

    Refactor components

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,77 @@
+import React, { FC } from 'react';
+import Image from 'next/image';
+import { styled } from '@mui/material/styles';
+import { IconButton, Toolbar, Typography } from '@mui/material';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
+interface AppBarWrapperProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+interface AppBarProps {
+  open?: boolean;
+  handleDrawerOpen: () => void;
+  handleDrawerClose: () => void;
+}
+
+const AppBarWrapper = styled(MuiAppBar, {
+  shouldForwardProp: prop => prop !== 'open',
+})<AppBarWrapperProps>(({ theme }) => ({
+  backgroundColor: 'white',
+  color: theme.palette.text.primary,
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+}));
+
+const AppBar: FC<AppBarProps> = ({
+  open,
+  handleDrawerOpen,
+  handleDrawerClose,
+}) => {
+  return (
+    <AppBarWrapper position="fixed" open={open}>
+      <Toolbar>
+        {open ? (
+          <IconButton
+            onClick={handleDrawerClose}
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2 }}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : (
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+        <Image
+          src={'/static/img/logo.png'}
+          width={40}
+          height={40}
+          alt="To Do App logo"
+          style={{
+            marginRight: '0.5rem',
+          }}
+        />
+        <Typography variant="h6" noWrap component="div">
+          <span style={{ fontWeight: 600 }}>To Do</span> app
+        </Typography>
+      </Toolbar>
+    </AppBarWrapper>
+  );
+};
+
+export default AppBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,64 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
 import Sidebar from '../components/Sidebar';
+import { styled } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { Box } from '@mui/system';
+import Cards from '../components/Cards';
+import { useState } from 'react';
+import { drawerWidth } from '../components/Sidebar/Sidebar';
+import AppBar from '../components/AppBar';
+
+const Main = styled('main', { shouldForwardProp: prop => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 2,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+const HeaderOffset = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
 
 const Home: NextPage = () => {
+  const [open, setOpen] = useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <Sidebar />
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar
+          open={open}
+          handleDrawerOpen={handleDrawerOpen}
+          handleDrawerClose={handleDrawerClose}
+        />
+        <Sidebar isOpen={open} />
+        <Main open={open}>
+          <HeaderOffset />
+          <Cards sidebarWidth={open ? drawerWidth : 0} />
+        </Main>
+      </Box>
     </>
   );
 };
