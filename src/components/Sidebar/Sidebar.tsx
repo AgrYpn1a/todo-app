9bfc96e410e00e971f7a904c907125aaf355f567
Rastko Tojagic <8715031+rtojagic@users.noreply.github.com>
2022-08-01 20:27:19 +0200

    Main layout, Sidebar, Cards layout (#8)
    * Define theme, typography
    
    * Main layout: Sidebar & Cards
     - Add main layout with sidebar
     - Add cards masonry layout
     - Add app logo
    
    * Refactor components

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,77 @@
+import React, { FC } from 'react';
+import { styled } from '@mui/material/styles';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import MailIcon from '@mui/icons-material/Mail';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+
+export const drawerWidth = 240;
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+interface SidebarProps {
+  isOpen: boolean;
+}
+
+const Sidebar: FC<SidebarProps> = ({ isOpen }) => {
+  // const theme = useTheme();
+
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="persistent"
+      anchor="left"
+      open={isOpen}
+    >
+      <DrawerHeader />
+      {/* <Divider /> */}
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      {/* <Divider /> */}
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import emotionReset from 'emotion-reset';
-import { Global, css } from '@emotion/react';
+import { Global, css, ThemeProvider } from '@emotion/react';
+import theme from '../lib/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,15 +9,23 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Global
         styles={css`
           ${emotionReset}
-          *, *::after, *::before {
+          *,
+          *::after,
+          *::before {
             box-sizing: border-box;
             -moz-osx-font-smoothing: grayscale;
             -webkit-font-smoothing: antialiased;
             font-smoothing: antialiased;
           }
+          body {
+            font-family: 'Montserrat', sans-serif;
+            background-color: lightgray;
+          }
         `}
       />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
